63b91aa0b4f0f0890ad606aa54cf956e7758e882
Svitwave <svitlanamaz@gmail.com>
2025-11-29 20:01:09 +0200

    fix pages notes

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,3 +1,36 @@
+// "use client";
+
+// import { useQuery } from "@tanstack/react-query";
+// import { fetchNoteById } from "@/lib/api";
+// import css from "./page.module.css";
+
+// type Props = {
+//   id: string;
+// };
+
+// export default function NoteDetailsClient({ id }: Props) {
+//   const { data, isLoading, error } = useQuery({
+//     queryKey: ["notes", id],
+//     queryFn: () => fetchNoteById(id),
+//     refetchOnMount: false,
+//   });
+
+//   if (isLoading) return <p>Loading, please wait...</p>;
+//   if (error || !data) return <p>Something went wrong.</p>;
+
+//   return (
+//     <div className={css.container}>
+//       <div className={css.item}>
+//         <div className={css.header}>
+//           <h2>{data?.title}</h2>
+//         </div>
+//         <p className={css.content}>{data?.content}</p>
+//         <p className={css.date}>{data?.createdAt}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
@@ -9,23 +42,32 @@ type Props = {
 };
 
 export default function NoteDetailsClient({ id }: Props) {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["notes", id],
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
   });
 
-  if (isLoading) return <p>Loading, please wait...</p>;
-  if (error || !data) return <p>Something went wrong.</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (error || !note) return <p>Something went wrong.</p>;
+
+  const formattedDate = note.updatedAt
+    ? `Updated at: ${note.updatedAt}`
+    : `Created at: ${note.createdAt}`;
 
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{data?.title}</h2>
+          <h2>{note.title}</h2>
         </div>
-        <p className={css.content}>{data?.content}</p>
-        <p className={css.date}>{data?.createdAt}</p>
+
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{formattedDate}</p>
       </div>
     </div>
   );
